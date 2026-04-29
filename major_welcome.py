1795ba830437f3a121d224aced54b95b800afaf4
jtpthompson <jtpthomp@byu.edu>
2026-04-29 10:08:00 -0600

    This is a simple test to make sure that my commit is working

--- major_welcome.py
+++ major_welcome.py
@@ -2,4 +2,4 @@ name = input("What is your name? ")
 major = input("What is your major? ")
 
 print(f"Welcome {name} to {major}!")
-print("Welcome " + name + " to " + major + "!")
+print("We hope you have a great time learning and growing in your field of study.")
